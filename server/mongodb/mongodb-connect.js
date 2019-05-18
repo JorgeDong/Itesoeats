828e7703dc5736576b792afbad559f67a1d60785
JorgeDong <Jorge>
2019-05-18 11:49:38 -0500

    'commit'

--- server/mongodb/mongodb-connect.js
+++ server/mongodb/mongodb-connect.js
@@ -1,7 +1,7 @@
 'use strict'
 
 let mongoose= require('mongoose');
-let mongoDB = 'mongodb+srv://is702396_:alvaro1.@cluster0-xftk7.mongodb.net/test?retryWrites=true'
+let mongoDB =process.env.MONGODB_URL ||  'mongodb+srv://is702396_:alvaro1.@cluster0-xftk7.mongodb.net/test?retryWrites=true';
 mongoose.connect(mongoDB,{
     useNewUrlParser: true,
     useCreateIndex: true});
